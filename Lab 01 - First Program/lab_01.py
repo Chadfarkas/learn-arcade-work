40df407536d50f9b1ebdf3d2e599b087eef980ce
chadfarkas <nightcrawler@pop-os.localdomain>
2022-04-18 16:15:33 +1000

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,7 @@
+print (""" \"Oh bliss, bliss and heaven
+... Oh, it was gorgeousness and gorgeousity made flesh... 
+And then, a bird of rarest-spun heaven metal, 
+or like silvery wine flowing in a spaceship, 
+gravity all nonsense now... I knew such lovely pictures\" """)
+
+
